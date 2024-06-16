5d7ba9ca7310b99db06a1eb0b4498dc8af56558f
sabinovas <90137248+sabinovas@users.noreply.github.com>
2024-06-17 00:27:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,9 +39,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ msg: "test route" });
